e8493817b506f4baf6ecd68cf808d0a6b5437e55
YoshihiroUeki <pirori67@gmail.com>
2019-09-07 23:42:19 +0900

    guide code

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+// https://capacitor.ionicframework.com/docs/guides/ionic-framework-app
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
-})
 export class Tab2Page {
+  photo: SafeResourceUrl;
 
-  constructor() {}
-
+  constructor(private sanitizer: DomSanitizer) {  }
 }
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-tab2',
+//   templateUrl: 'tab2.page.html',
+//   styleUrls: ['tab2.page.scss']
+// })
+// export class Tab2Page {
+
+//   constructor() {}
+
+// }
